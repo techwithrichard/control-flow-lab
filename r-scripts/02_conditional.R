10a358d88e205fdc27e36b078c46693056c18eef
techwithrichard <richardkoech69@gmail.com>
2025-09-01 15:42:24 +0300

    added conditional statment with If function

---
+++ r-scripts/02_conditional.R
@@ -0,0 +1,17 @@
+# CONDITIONAL STATEMENTS
+# If, else if, else => executed when a condition is TRUE
+# Switch => used to select one of many alternatives based on the value of an expression
+
+cat("Conditional Statements Examples here", "\n")
+# load data/
+cat("Step 1: Loading iris dataset", "\n")
+data("iris")
+
+cat("Step 2: Displaying the first few rows of the dataset", "\n")
+head(iris)
+
+cat("Step 3: If Function", "\n")
+sepal_length <- mean(iris$Sepal.Length)
+if (sepal_length > 5) {
+  cat("The average sepal length is greater than 5\n")
+}
